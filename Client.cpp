0d4daadf85f46b81b04892867a217e71052ebfc1
Abdullah <abdullah@Abdullahs-MacBook-Pro.local>
2020-11-29 13:49:22 +0500

    select code update

--- Client.cpp
+++ Client.cpp
@@ -1,16 +1,42 @@
-#include <iostream>
-using namespace std;
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<assert.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
 
-int main(){
-    cout<<"Enter the client IP which you would like to talk to: ";
-    string friendIP;
-    getline(cin, friendIP, '\n');
+int main()
+{
+	int sockfd = socket(AF_INET,SOCK_STREAM,0);	
+	assert(sockfd != -1 );
 
-    cout<<endl;
+	//Set Address Information
+	struct sockaddr_in saddr;
+	memset(&saddr,0,sizeof(saddr));
+	saddr.sin_family = AF_INET;
+	saddr.sin_port = htons(6000);
+	saddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    string message;
-    do{
-    cout<<"YOU: ";    
-    getline(cin, message, '\n');
-    }    while (message != "close");
+	//Link to server
+	int res = connect(sockfd,(struct sockaddr*)&saddr,sizeof(saddr));
+	assert(res != -1);
+
+	while(1)
+	{
+		char buff[128] = {0};
+		printf("Please Input:");
+		fgets(buff,128,stdin);
+		if(strncmp(buff,"end",3) ==0 )
+		{
+			break;
+		}
+		send(sockfd,buff,strlen(buff),0);
+		memset(buff,0,128);
+		recv(sockfd,buff,127,0);
+		printf("RecvBuff:%s\n",buff);
+        printf("\n");
+	}
+	close(sockfd);
 }
